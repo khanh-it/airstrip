dbce92ec3e11be55e2e0a36711da622abe8501ff
pk_home <phikhanh.8x@gmail.com>
2016-10-03 15:51:54 +0700

    'xwf-call' library!

--- www/js/iframe.js
+++ www/js/iframe.js
@@ -1,30 +1,29 @@
-
-var objVar = {
-	'prop_01' : 'value_01',
-	'prop_02' : ['value_01'],
-	'prop_03' : {
-		'i-prop_01' : 'i-value_01',
-	}/*,
-	'func_01' : function(){
-		alert(this.prop_01);
-		return {'func_02': function(){
-			alert('def');
-		}};
-	}*/
-};
-console.time('benchmark_XCalls');
-xwfCall(function(param1){
-	console.log('abc');
-	//console.log(window);
-	//console.log(window.cordova.version());
-	
-	return 'some-value';
-}, objVar)(function(result){
-	console.log('result: ', result);
-});
-/*(function(param1){
-	console.log(param1);
-	//console.log(window);
-	//console.log(window.cordova.version());
-})(objVar);*/
-console.timeEnd('benchmark_XCalls');
+(function($){
+	var objVar = {
+		'prop_01' : 'value_01',
+		'prop_02' : ['value_01'],
+		'prop_03' : {
+			'i-prop_01' : 'i-value_01',
+		}/*,
+		'func_01' : function(){
+			alert(this.prop_01);
+			return {'func_02': function(){
+				alert('def');
+			}};
+		}*/
+	};
+	console.time('benchmark_XCalls');
+	xwfCall(function(param1){
+		delete param1.prop_03;
+		return;
+		//console.log(window.cordova.version());
+		$.get('http://192.168.1.64/airstrip/www/index.html', null, function(rs){
+			xwfCall(function(html){
+				console.log('html: ', html);
+			}, null, rs);
+		}, 'html');
+		return 'some-value';
+	}, null, objVar);
+	console.log(objVar);
+	console.timeEnd('benchmark_XCalls');
+})(jQuery);
--- www/js/xwf-call.js
+++ www/js/xwf-call.js
@@ -8,28 +8,45 @@
  */
 (function(_win, _xWin){
 	// @var {String} postMessage origin.
-	var PM_ORIGIN = 'http://10.10.8.169';
-	// @var {String} postMessage message key.
-	var PM_MSG_KEY = '-/-XWF-CALL-/-';
+	var ORIGIN = 'http://192.168.1.64';
+	// @var {String} postMessage message key for request.
+	var MSG_KEY_REQ = '-#XWF-REQ#-';
+	// @var {String} postMessage message key for response.
+	var MSG_KEY_RES = '-#XWF-RES#-';
 	// @var {Boolean} Is cross windows flag?
 	var isXW = (_win !== _xWin);
 	// @var {Function} Event handler
 	// Handle cross function calls  
 	var onMessageEvtHandler = function(evt){
-		if (evt.origin != PM_ORIGIN) {
+		console.log('postMessage...', '' + evt.origin);
+		if (evt.origin != ORIGIN) {
 			console && console.log('[xwf-call] Origin does not match.'); 
 			return;
 		}
-		if (('string' == typeof(evt.data))
-			&& (0 == evt.data.indexOf(PM_MSG_KEY))
-		) {
+		// Case: request calls...
+		if ((evt.data.indexOf(MSG_KEY_REQ) >= 0)) {
+			console.log('MSG_KEY_REQ: ', MSG_KEY_REQ);
 			// Format function string
-			var func = evt.data.replace(PM_MSG_KEY, '');
+			var data = evt.data.split(MSG_KEY_REQ),
+				cbID = data[0], 
+				func = data[1],
+				result
+			;
             // Execute temp function
-            eval(func);
+            eval('result = ' + func + ';');
+            // Has callback?
+            if (cbID) {
+            	console.log('cbID: ', cbID);
+            	evt.source.postMessage(cbID + MSG_KEY_RES, evt.origin);
+            }
 		}
+		// Case: response calls...
+		if ((evt.data.indexOf(MSG_KEY_RES) >= 0)) {
+			
+		}
+		
 		// Self destruct
-		(_win.removeEventListener || _win.detachEvent)('message', onMessageEvtHandler, false);
+		//(_win.removeEventListener || _win.detachEvent)('message', onMessageEvtHandler, false);
 	};
 	// Register event handler, handle cross function calls...  
 	(_win.addEventListener || _win.attachEvent)('message', onMessageEvtHandler, false);
@@ -53,34 +70,52 @@
 		}
 	};
 	
+	// @var {Object} Hold postMessage callbacks
+	var PM_Callbacks = {};
+	/**
+	 * Register callback
+	 * 
+	 * @param callback {Function} A callback function 
+	 * @return {String} Register id
+	 */
+	var registerCallback = function(callback) {
+		if ('function' == (typeof callback)) {
+			// Generate id
+			var id = '_' + (new Date()).getTime();
+			// Register callback
+			PM_Callbacks[id] = callback;
+			// Return
+			return id;
+		}
+	};
+	
 	/**
 	 * Cross windows function call
 	 * @return void
 	 */
-	_win.xwfCall = _win.xwfCall || function(){
-		// Convert `arguments` to array
-		arguments = Array.prototype.slice.call(arguments);
-		
-		var func = arguments.shift();
+	_win.xwfCall = _win.xwfCall || function(func, callback){
 		if ('function' == (typeof func)) {
-			var result = undefined, 
-				rtn = function(){
-					
-				}
-			;
+			// Get params from `arguments`
+			arguments = Array.prototype.slice.call(arguments, 2);
+			// Case: same window calls
 			if (!isXW) {
-				result = func.apply(undefined, arguments);
+				(callback || function(){})(
+					func.apply(undefined, arguments)
+				);
 			// Case: cross window calls
 			} else {
 				// Convert function to string  
 				func = func2StrPadding(func, arguments);
-				// Forward function call
 				if (func) {
-					_xWin.postMessage(PM_MSG_KEY + func, '*');
-				} 
+					// Register callback?
+					var cbID = '';
+					if ('function' == (typeof callback)) {
+						cbID = registerCallback(callback);
+					}
+					// Forward function call 
+					_xWin.postMessage(cbID + MSG_KEY_REQ + func, '*');
+				}
 			}
-			// Return
-			
 		}
 	};
 })(window, window.opener || window.parent);
